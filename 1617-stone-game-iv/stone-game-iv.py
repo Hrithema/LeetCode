f369a5644df6d8af97c70bd254467d112fe57736
Hrithema <hrithema@gmail.com>
2026-08-11 00:59:14 +0530

    Time: 1789 ms (15.76%) | Memory: 20 MB (85.15%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.py
@@ -0,0 +1,10 @@
+class Solution:
+    def winnerSquareGame(self, n: int) -> bool:
+        ans = [False] * (n+1)
+        for i in range (1, n+1):
+            for j in range(1, math.isqrt(i)+1):
+                square = j* j
+                if not ans [i - square]:
+                    ans [i] = True
+                    # break
+        return ans [n]
