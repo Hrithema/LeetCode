6574edb559de4d51fb3c62d49774d4aed24425b9
Hrithema <hrithema@gmail.com>
2026-08-25 17:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.35%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -1,5 +1,6 @@
 class Solution:
     def missingMultiple(self, nums: List[int], k: int) -> int:
+        nums = set(nums)
         for i in range (1, len(nums) + 2):
             n = k * i
             if n not in nums:
